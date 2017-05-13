a0bb7135cda12310721b4a1f964a6e4e0e048fd4
15017199 <15017199@myrp.edu.sg>
2017-05-14 00:43:53 +0800

    - 2nd change

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -1,17 +1,20 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.myapplicationdev.android.p04_revisionnotes.R.id.lv;
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     RadioButton r1,r2,r3,r4,r5;
     Button btnInsert;
     Button btnShow;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
                 // Create the DBHelper object, passing in the
                 // activity's Context
                 DBHelper db = new DBHelper(MainActivity.this);
-
                 // Insert a task
                 String insert = edNote.getText().toString();
                 db.insertNote(insert,1);
@@ -60,7 +63,31 @@ public class MainActivity extends AppCompatActivity {
                 db.close();
             }
         });
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+                // Insert a task
+                ArrayList<String> data = db.getNoteContent();
+                db.close();
 
+				String txt = "";
+				for (int i = 0; i < data.size(); i++) {
+					Log.d("Database Content", i + ". " + data.get(i));
+					txt += i + ". " + data.get(i) + "\n";
+				}
+				// lv.setText(txt);
+
+                DBHelper db2 = new DBHelper(MainActivity.this);
+                ArrayList<Note> al = db2.getAllNotes();
+                db2.close();
+
+                RevisionNotesArrayAdapter aa = new RevisionNotesArrayAdapter(SecondActivity.this,R.layout.row,al);
+                lv.setAdapter(aa);
+            }
+        });
 
     }
 }
